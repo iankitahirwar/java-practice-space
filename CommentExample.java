9021cc0e928d84e07158f6b2685ccb6ceb315ba8
Ankit Ahirwar <0801it243d01@sgsits.ac.in>
2026-01-02 23:40:38 +0530

    Comments
    Comments single line & multi line comments in the code

---
+++ CommentExample.java
@@ -0,0 +1,13 @@
+public class CommentExample {
+  public static void main(String[] args) {
+		
+	// this line is comment-for Single Line 
+    System.out.println("Hello world!");
+	
+    /*	String name = "Ankit";
+	System.out.println("Hey" + name);
+
+	*/ 
+	//For multi Line Comments
+  }
+}
