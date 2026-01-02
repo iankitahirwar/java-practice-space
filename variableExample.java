f80712328e30d5160aee399b104ca91ef1ffe195
Ankit Ahirwar <0801it243d01@sgsits.ac.in>
2026-01-02 23:41:54 +0530

    Variable Example
    Create a integer , String & float variable

---
+++ variableExample.java
@@ -0,0 +1,11 @@
+public class variableExample {
+  public static void main(String[] args) {
+
+    int num1 = 5;
+    float num2 = 59.12f ;
+     String text = "Hello" ; 
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(text);
+  }
+}
