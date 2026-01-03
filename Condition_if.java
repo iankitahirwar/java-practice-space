7af7946ab99f3dc8bf0e7e0399dbc0e557f4589d
Ankit Ahirwar <0801it243d01@sgsits.ac.in>
2026-01-03 23:54:51 +0530

    if Condition Statement
    Learning Condition Statement

---
+++ Condition_if.java
@@ -0,0 +1,10 @@
+class Condition_if {
+    public static void main(String args[])
+    {
+        int i = 10;
+
+        if (i < 15) {
+            System.out.println("Condition is True");
+        }
+    }
+}
